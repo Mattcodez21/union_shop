d827d2215ee5a493c685c76565f15e823d387e37
Mattcodez21 <166388565+Mattcodez21@users.noreply.github.com>
2025-11-30 16:48:13 +0000

    Corrected about page to wrap column in a SingleChildScrollView

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -9,31 +9,29 @@ class AboutPage extends StatelessWidget {
     return Scaffold(
       appBar: const Navbar(title: 'About'),
       endDrawer: const MobileNavDrawer(),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'About Us',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'About Us',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 24),
-              Text(
-                'Welcome to the Union Shop!\n\n'
-                'We\'re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!\n\n'
-                'All online purchases are available for delivery or instore collection!\n\n'
-                'We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don\'t hesitate to contact us at hello@upsu.net.\n\n'
-                'Happy shopping!\n\n'
-                'The Union Shop & Reception Team',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 24),
+            Text(
+              'Welcome to the Union Shop!\n\n'
+              'We\'re dedicated to giving you the very best University branded products, with a range of clothing and merchandise available to shop all year round! We even offer an exclusive personalisation service!\n\n'
+              'All online purchases are available for delivery or instore collection!\n\n'
+              'We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don\'t hesitate to contact us at hello@upsu.net.\n\n'
+              'Happy shopping!\n\n'
+              'The Union Shop & Reception Team',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ],
         ),
       ),
     );
